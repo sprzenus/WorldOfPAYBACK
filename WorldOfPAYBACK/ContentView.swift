870d2e551fe8ffd9d7ec636053cbe994e525170d
Bartłomiej Świerad <bartlomiej.swierad@gmail.com>
2023-10-21 08:16:33 +0200

    Initial Commit

---
+++ WorldOfPAYBACK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorldOfPAYBACK
+//
+//  Created by Bartłomiej Świerad on 21/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorldOfPAYBACK/WorldOfPAYBACKApp.swift
@@ -0,0 +1,19 @@
+//
+//  WorldOfPAYBACKApp.swift
+//  WorldOfPAYBACK
+//
+//  Created by Bartłomiej Świerad on 21/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPAYBACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabView {
+                ContentView()
+            }
+        }
+    }
+}
